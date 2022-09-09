3a86558fdb39e47e5112de02678bb260b9b77316
Tobias Hecker <heckertobias@googlemail.com>
2022-09-09 21:25:33 +0200

    add eslint to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('app');
-const root = createRoot(container!);
+const container = document.getElementById("app");
+const root = createRoot(container);
 root.render(<h1>Hello World!</h1>);
